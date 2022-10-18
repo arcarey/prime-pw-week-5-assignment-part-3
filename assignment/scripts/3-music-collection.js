0d61c68790c4a16dd2fc0422968eca7df151bc87
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-17 21:06:39 -0500

    completed first two bullet points

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+
+// initiate the music collection
+let collection = [];
+
+// create function that adds music info to the collection array
+function addToCollection(title, artist, yearPublished) {
+    let album = {
+        'title': title,
+        'artist': artist,
+        'year published': yearPublished
+    } // end album
+    collection.push (album);
+    return album;
+} // addToCollection
+
+
